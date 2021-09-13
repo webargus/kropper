aab0a09b0089ee1062eede1f108af032cdb47870
Edson Kropniczki <kropniczki@gmail.com>
2021-09-13 15:02:44 -0300

    honed crop area resizing code

--- js/kropper.js
+++ js/kropper.js
@@ -1,38 +1,42 @@
 
 const Kropper = (_ => {
     // globals
-    const img = document.createElement("img");
     const shadowboard = document.createElement("div");
     shadowboard.className = "kropper-shadowboard";
     const clipboard = document.createElement("div");
     clipboard.className = "kropper-clipboard";
-    clipboard.style.clipPath = "inset(16px 16px 16px 16px)";
     // clipboard resizers
     const resizerLeft = document.createElement("div");
-    resizerLeft.className = "kropper-handle";
+    resizerLeft.className = "kropper-resizer";
     const resizerBottom = document.createElement("div");
-    resizerBottom.className = "kropper-handle";
+    resizerBottom.className = "kropper-resizer";
     const resizerRight = document.createElement("div");
-    resizerRight.className = "kropper-handle";
+    resizerRight.className = "kropper-resizer";
     const resizerTop = document.createElement("div");
-    resizerTop.className = "kropper-handle";
-    var container, rect, dragHandle;
+    resizerTop.className = "kropper-resizer";
+    //  DOM image to hold user-submitted image for cropping
+    const img = document.createElement("img");
+    var container,      // user-defined element
+    userRect,            // container JS bounding rect
+    imgRect;             // JS bounding rect of img loaded via crop function
 
     const resetResizers = _  => {
         resizerLeft.style.top = resizerRight.style.top = resizerTop.style.left = resizerBottom.style.left = '50%';
         resizerLeft.style.left = resizerRight.style.right = resizerTop.style.top = resizerBottom.style.bottom = '16px';
+        clipboard.style.clipPath = "inset(16px 16px 16px 16px)";
     };
 
     const crop = uri => {
         img.src = uri;
         img.onload = _ => {
             if(img.width >= img.height) {
-                container.style.width = `${rect.width}px`;
-                container.style.height = `${(img.height/img.width)*rect.height}px`;
+                container.style.width = `${userRect.width}px`;
+                container.style.height = `${(img.height/img.width)*userRect.height}px`;
             } else {
-                container.style.height = `${rect.height}px`;
-                container.style.width = `${(img.width/img.height)*rect.width}px`;
+                container.style.height = `${userRect.height}px`;
+                container.style.width = `${(img.width/img.height)*userRect.width}px`;
             }
+            imgRect = container.getBoundingClientRect();
             img.style.aspectRatio = 'auto';
             shadowboard.style.backgroundImage = `url(${img.src})`;
             clipboard.style.backgroundImage = `url(${img.src})`;
@@ -57,7 +61,7 @@ const Kropper = (_ => {
     const create = (containerElement) => {
 
         container = containerElement;
-        rect = container.getBoundingClientRect();
+        userRect = container.getBoundingClientRect();
         container.appendChild(shadowboard);
         container.appendChild(clipboard);
         container.appendChild(resizerLeft);
@@ -65,10 +69,10 @@ const Kropper = (_ => {
         container.appendChild(resizerRight);
         container.appendChild(resizerTop);
 
-        container.querySelectorAll(".kropper-handle").forEach(resizer => {
+        container.querySelectorAll(".kropper-resizer").forEach(resizer => {
 
             const mousedown = e => {
-                dragHandle = e.target;
+                var dragResizer = e.target;
                 
                 const mousemove = e => {
                     let el = e.target;
@@ -76,9 +80,11 @@ const Kropper = (_ => {
                     let dragY = e.offsetY;
 
                     [topY, rightX, bottomY, leftX] = getClipPathArray();
-                    switch(dragHandle) {
+                    switch(dragResizer) {
                         case resizerLeft:
-                            if(el == dragHandle) {
+                            if(leftX > imgRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
+                                leftX = imgRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1;
+                            } else if(el == dragResizer) {
                                leftX--;
                             } else {
                                 leftX += (dragX > leftX) ? 1 : -1;
@@ -86,23 +92,27 @@ const Kropper = (_ => {
                             if(leftX < 0) {
                                 leftX = 0;
                             }
-                            dragHandle.style.left = leftX + "px";
-                            resizerTop.style.left = resizerBottom.style.left = leftX + (rect.width - leftX - rightX)/2 + "px";
+                            dragResizer.style.left = leftX + "px";
+                            resizerTop.style.left = resizerBottom.style.left = leftX + (imgRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
                             break;
                         case resizerRight:
-                            if(el == dragHandle) {
+                            if( rightX > imgRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
+                                rightX = imgRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1
+                            } else if(el == dragResizer) {
                                 rightX--;
                             } else {
-                                rightX += (dragX > rect.width - rightX) ? -1 : 1;
+                                rightX += (dragX > imgRect.width - rightX) ? -1 : 1;
                             }
                             if(rightX < 0) {
                                 rightX = 0;
                             }
-                            dragHandle.style.right = rightX + "px";
-                            resizerTop.style.left = resizerBottom.style.left = leftX + (rect.width - leftX - rightX)/2 + "px";
+                            dragResizer.style.right = rightX + "px";
+                            resizerTop.style.left = resizerBottom.style.left = leftX + (imgRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
                             break;
                         case resizerTop:
-                            if(el == dragHandle) {
+                            if(topY > imgRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
+                                topY = imgRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
+                            } else if(el == dragResizer) {
                                 topY--;
                             } else {
                                 topY += ( dragY > topY) ? 1 : -1;
@@ -110,20 +120,22 @@ const Kropper = (_ => {
                             if(topY < 0) {
                                 topY = 0;
                             }
-                            dragHandle.style.top = topY + "px";
-                            resizerLeft.style.top = resizerRight.style.top = topY + (rect.height - topY - bottomY)/2 + "px";
+                            dragResizer.style.top = topY + "px";
+                            resizerLeft.style.top = resizerRight.style.top = topY + (imgRect.height - topY - bottomY)/2 - resizerRight.offsetHeight/2 + "px";
                             break;
                         case resizerBottom:
-                            if(el == dragHandle) {
+                            if( bottomY > imgRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
+                                bottomY = imgRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
+                            } else if(el == dragResizer) {
                                 bottomY++;
                             } else {
-                                bottomY += ( dragY < rect.height - bottomY ) ? 1 : -1;
+                                bottomY += ( dragY < imgRect.height - bottomY ) ? 1 : -1;
                             }
                             if(bottomY < 0) {
                                 bottomY = 0;
                             }
-                            dragHandle.style.bottom = bottomY + "px";
-                            resizerLeft.style.top = resizerRight.style.top = topY + (rect.height - topY - bottomY)/2 + "px";
+                            dragResizer.style.bottom = bottomY + "px";
+                            resizerLeft.style.top = resizerRight.style.top = topY + (imgRect.height - topY - bottomY)/2 - resizerRight.offsetWidth/2 + "px";
                             break;
                     }
                     clipboard.style.clipPath = `inset(${[topY + 'px', rightX + 'px', bottomY + 'px', leftX + 'px'].join(' ')})`;
