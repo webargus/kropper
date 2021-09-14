afcb540f684008f7f50d3a80dc7a672eaea93a1d
Edson Kropniczki <kropniczki@gmail.com>
2021-09-14 16:31:40 -0300

    working on crop precision

--- js/kropper.js
+++ js/kropper.js
@@ -18,36 +18,13 @@ const Kropper = (_ => {
     const domImage = document.createElement("img");
     var container,      // user-defined element
     userRect,            // container JS bounding rect
-    imgRect;             // JS scaled bounding rect of img loaded via crop function
+    cropRect;             // JS scaled bounding rect of img loaded via crop function
 
     const reset = _  => {
         resizerLeft.style.top = resizerRight.style.top = resizerTop.style.left = resizerBottom.style.left = '50%';
         resizerLeft.style.left = resizerRight.style.right = resizerTop.style.top = resizerBottom.style.bottom = '16px';
         clipboard.style.clipPath = "inset(16px 16px 16px 16px)";
         CropObj.update(16, 16, 16, 16);
-    };
-
-    const crop = uri => {
-        return new Promise((resolve, reject) => {
-
-            domImage.src = uri;
-            domImage.onload = _ => {
-                if(domImage.width >= domImage.height) {
-                    container.style.width = `${userRect.width}px`;
-                    container.style.height = `${(domImage.height/domImage.width)*userRect.height}px`;
-                } else {
-                    container.style.height = `${userRect.height}px`;
-                    container.style.width = `${(domImage.width/domImage.height)*userRect.width}px`;
-                }
-                imgRect = container.getBoundingClientRect();
-                domImage.style.aspectRatio = 'auto';
-                shadowboard.style.backgroundImage = `url(${domImage.src})`;
-                clipboard.style.backgroundImage = `url(${domImage.src})`;
-                reset();
-                resolve("ready");
-            };
-        });
-
     };
 
     const CropObj = (_ => {
@@ -57,26 +34,67 @@ const Kropper = (_ => {
         var sCropWidth,       // width of crop region in source image
         sCropHeight,            // height of crop region in souce image
         sX, sY;                     // coords of top left crop rectangle in source image
+        var cropWidth, cropHeight
+    
+        const crop = uri => {
+            return new Promise((resolve, reject) => {
+    
+                domImage.src = uri;
+                domImage.onload = _ => {
+                    if(domImage.width >= domImage.height) {
+                        container.style.width = `${userRect.width}px`;
+                        container.style.height = `${(domImage.height/domImage.width)*userRect.height}px`;
+                    } else {
+                        container.style.height = `${userRect.height}px`;
+                        container.style.width = `${(domImage.width/domImage.height)*userRect.width}px`;
+                    }
+                    cropRect = container.getBoundingClientRect();
+                    console.log("cropRect", cropRect);
+                    console.log("domImage.width", domImage.width, "domImage.height", domImage.height);
+                    canvasContext.width = userRect.width;
+                    canvasContext.height = userRect.height;
+                    canvasContext.drawImage(domImage, 0, 0, canvasContext.width, canvasContext.height);
+                    //domImage.style.aspectRatio = 'auto';
+                    canvas.toBlob( blob => {
+                        const uri = URL.createObjectURL(blob);
+                        shadowboard.style.backgroundImage = uri; //`url(${uri})`;
+                        clipboard.style.backgroundImage = uri; //`url(${uri})`;
+                        URL.revokeObjectURL(uri);
+                        reset();
+                        resolve("ready");
+                    }, 'image/png', 1);
+                };
+            });
+    
+        };
     
         const update = (left, top, right, bottom)  => {
             // calc width and height of clipboard img
-            const cropWidth = imgRect.width - left - right;
-            const cropHeight = imgRect.height - top - bottom;
+            cropWidth = cropRect.width - left - right;
+            cropHeight = cropRect.height - top - bottom;
             // calc corresponding width and height in source img
-            sCropWidth = cropWidth * domImage.width / imgRect.width;
-            sCropHeight = cropHeight * domImage.height / imgRect.height;
+            sCropWidth = cropWidth * domImage.width / cropRect.width;
+            sCropHeight = cropHeight * domImage.height / cropRect.height;
             // calc top left coords of img to crop in source img
-            sX = left * domImage.width / imgRect.width;
-            sY = top * domImage.height / imgRect.height;
+            sX = left * domImage.width / cropRect.width;
+            sY = top * domImage.height / cropRect.height;
             // trigger custom oncrop event
             const event = new CustomEvent("crop", { detail: {x: sX, y: sY, width: sCropWidth, height: sCropHeight } });
             dispatchEvent(event);
         };
 
         const result = callback => {
+            console.log("sX", sX, "sY", sY);
+            console.log("cropWidth", cropWidth, "cropHeight", cropHeight);
+            console.log("sCropWidth", sCropWidth, "sCropHeight", sCropHeight);
+            console.log("domImg.width", domImage.width, "domImg.height", domImage.height);
+            console.log("cropRect.width", cropRect.width, "cropRect.height", cropRect.height);
+            console.log("sCropWidth/domImg.width", sCropWidth/domImage.width);
+            console.log("cropWidth/cropRect.width", cropWidth/cropRect.width);
+            console.log("cropHeight/cropRect.height", cropHeight/cropRect.height);
             canvas.width = sCropWidth;
             canvas.height = sCropHeight;
-            canvasContext.drawImage(domImage, sX, sY, canvas.width, canvas.height);
+            canvasContext.drawImage(domImage, sX, sY,  sCropWidth, sCropHeight, 0, 0, sCropWidth, sCropHeight);
             canvas.toBlob( blob => {
                 callback(blob);
             }, "image/png", 1);
@@ -84,6 +102,7 @@ const Kropper = (_ => {
 
         return {
             update: update,
+            crop: crop,
             result: result
         };
     })();
@@ -126,8 +145,8 @@ const Kropper = (_ => {
                     [topY, rightX, bottomY, leftX] = getClipPathArray();
                     switch(dragResizer) {
                         case resizerLeft:
-                            if(leftX > imgRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
-                                leftX = imgRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1;
+                            if(leftX > cropRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
+                                leftX = cropRect.width - rightX -  resizerLeft.offsetWidth - resizerRight.offsetWidth - 1;
                             } else if(el == dragResizer) {
                                leftX--;
                             } else {
@@ -137,25 +156,25 @@ const Kropper = (_ => {
                                 leftX = 0;
                             }
                             dragResizer.style.left = leftX + "px";
-                            resizerTop.style.left = resizerBottom.style.left = leftX + (imgRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
+                            resizerTop.style.left = resizerBottom.style.left = leftX + (cropRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
                             break;
                         case resizerRight:
-                            if( rightX > imgRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
-                                rightX = imgRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1
+                            if( rightX > cropRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1) {
+                                rightX = cropRect.width - leftX - resizerLeft.offsetWidth - resizerRight.offsetWidth - 1
                             } else if(el == dragResizer) {
                                 rightX--;
                             } else {
-                                rightX += (dragX > imgRect.width - rightX) ? -1 : 1;
+                                rightX += (dragX > cropRect.width - rightX) ? -1 : 1;
                             }
                             if(rightX < 0) {
                                 rightX = 0;
                             }
                             dragResizer.style.right = rightX + "px";
-                            resizerTop.style.left = resizerBottom.style.left = leftX + (imgRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
+                            resizerTop.style.left = resizerBottom.style.left = leftX + (cropRect.width - leftX - rightX)/2 - resizerBottom.offsetWidth/2 + "px";
                             break;
                         case resizerTop:
-                            if(topY > imgRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
-                                topY = imgRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
+                            if(topY > cropRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
+                                topY = cropRect.height - bottomY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
                             } else if(el == dragResizer) {
                                 topY--;
                             } else {
@@ -165,21 +184,21 @@ const Kropper = (_ => {
                                 topY = 0;
                             }
                             dragResizer.style.top = topY + "px";
-                            resizerLeft.style.top = resizerRight.style.top = topY + (imgRect.height - topY - bottomY)/2 - resizerRight.offsetHeight/2 + "px";
+                            resizerLeft.style.top = resizerRight.style.top = topY + (cropRect.height - topY - bottomY)/2 - resizerRight.offsetHeight/2 + "px";
                             break;
                         case resizerBottom:
-                            if( bottomY > imgRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
-                                bottomY = imgRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
+                            if( bottomY > cropRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1) {
+                                bottomY = cropRect.height - topY - resizerTop.offsetHeight - resizerBottom.offsetHeight - 1;
                             } else if(el == dragResizer) {
                                 bottomY++;
                             } else {
-                                bottomY += ( dragY < imgRect.height - bottomY ) ? 1 : -1;
+                                bottomY += ( dragY < cropRect.height - bottomY ) ? 1 : -1;
                             }
                             if(bottomY < 0) {
                                 bottomY = 0;
                             }
                             dragResizer.style.bottom = bottomY + "px";
-                            resizerLeft.style.top = resizerRight.style.top = topY + (imgRect.height - topY - bottomY)/2 - resizerRight.offsetWidth/2 + "px";
+                            resizerLeft.style.top = resizerRight.style.top = topY + (cropRect.height - topY - bottomY)/2 - resizerRight.offsetWidth/2 + "px";
                             break;
                     }
                     clipboard.style.clipPath = `inset(${[topY + 'px', rightX + 'px', bottomY + 'px', leftX + 'px'].join(' ')})`;
@@ -204,7 +223,7 @@ const Kropper = (_ => {
 
     return {
         create: create,
-        crop: crop,
+        crop: CropObj.crop,
         result: CropObj.result,
      };
 
